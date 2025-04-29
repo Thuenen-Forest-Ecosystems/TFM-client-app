cfd0582f41627732157c35a7930ea1551f14b1bf
b-lack <b-lack@users.noreply.github.com>
2025-04-29 13:05:18 +0000

    deploy: 450de848a6f8c3b0414fdd58242f05a13c850e56

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2548655573"
+    serviceWorkerVersion: "3358934914"
   }
 });
